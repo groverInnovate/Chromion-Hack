6c2253d01accc47a80ac2385465f168e4444095c
Abhivansh Saini <31abhivanshj@gmail.com>
2025-06-16 15:52:57 +0000

    factory contract rough implementation done

--- contracts/src/Agent.sol
+++ contracts/src/Agent.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract Agent {}
+contract Agent {
+
+    error Agent__LengthNotSame();
+
+    string[] tokens;
+    string platformType;
+
+    constructor(string[] memory _tokens, string memory _platformType) {
+        if (tokens.length != _tokens.length) {
+            revert Agent__LengthNotSame();
+        }
+        for (uint256 i = 0; i < tokens.length; i++ ) {
+            tokens[i] = _tokens[i];
+        }
+        platformType = _platformType;
+    }
+
+    function getPlatform() external view returns(string memory) {
+        return platformType;
+    }
+
+    function getTokens() external view returns(string[] memory) {
+        return tokens;
+    }
+}
--- contracts/src/AgentFactory.sol
+++ contracts/src/AgentFactory.sol
@@ -1,4 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract AgentFactory {}
+import {Agent} from './Agent.sol';
+
+contract AgentFactory {
+
+    uint256 constant public MAX_AGENTS_LIMIT = 3; // one for each platform
+
+    error Factory__NoTokenPresent();
+    error Factory__PlatformNotAvailable();
+    error Factory__AmountIsZero();
+
+    enum PlatformType {
+        Twitter,
+        Telegram,
+        Discord
+    }
+
+    struct AgentInfo {
+        address agentAddress;
+        address owner;
+        string[] tokens;
+        uint256 createdAt;
+        uint256 amountInvested;
+        string PlatformType;
+    }
+
+    mapping (address user => AgentInfo[]) userToAgents;
+    mapping (address user => uint256 number) userToNumberOfAgents;
+
+    function createAgent(string[] memory _tokens, string memory _platformType) external payable {
+        if (_tokens.length == 0) {
+            revert Factory__NoTokenPresent();
+        }
+        if (keccak256(bytes(_platformType)) != keccak256(bytes("Telegram")) && 
+            keccak256(bytes(_platformType)) != keccak256(bytes("Twitter")) && 
+            keccak256(bytes(_platformType)) != keccak256(bytes("Discord"))) {
+            revert Factory__PlatformNotAvailable();
+        }
+        if (msg.value <= 0) {
+            revert Factory__AmountIsZero();
+        }
+
+        Agent agent = new Agent(_tokens, _platformType);
+
+        AgentInfo memory info = AgentInfo({
+            agentAddress: address(agent),
+            owner: msg.sender,
+            tokens: _tokens,
+            createdAt: block.timestamp,
+            amountInvested: msg.value,
+            PlatformType: _platformType
+        });
+
+        userToAgents[msg.sender].push(info);
+        userToNumberOfAgents[msg.sender] += 1;
+    }   
+
+    function getNumberofAgentsForUser(address user) external view returns(uint256) {
+        return userToNumberOfAgents[user];
+    }
+
+    function getAgentsForUser(address user) external view returns(AgentInfo[] memory) {
+        return userToAgents[user];
+    }
+}
