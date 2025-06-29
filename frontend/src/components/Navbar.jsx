341fe2cd5ec3b5e8bae697d81b0f1af5f36cbb37
dxkx03 <devanshi_k@es.iitr.ac.in>
2025-06-30 02:07:44 +0530

    logo done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useHelpBox } from "./HelpBox.jsx";
-import {useWallet} from "../context/WalletContext.jsx";
+import { useWallet } from "../context/WalletContext.jsx";
+import logo from "../assets/TradeSynth.png";
 
 const Navbar = () => {
 
   const [currentPath, setCurrentPath] = useState("");
   const { openHelp } = useHelpBox();
-  const{address,connectWallet}=useWallet();
-  
+  const { address, connectWallet } = useWallet();
+
   useEffect(() => {
     setCurrentPath(window.location.pathname);
   }, []);
@@ -18,7 +19,9 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center px-6 md:px-16 py-4 absolute top-0 left-0 w-full z-20">
-      <div className="text-white text-xl font-semibold tracking-widest">LOGO</div>
+      <div className="flex items-center gap-2">
+        <img src={logo} alt="Tradesynth Logo" className="w-10 h-10 object-contain" />
+      </div>
 
       <div className="hidden md:flex gap-8 text-white text-sm md:text-base">
         <a href="/profile" className={linkClasses("/profile")} >Profile</a>
@@ -33,8 +36,8 @@ const Navbar = () => {
 
       <div className="flex items-center gap-4">
         <button onClick={connectWallet} disabled={!!address} className="bg-white text-black px-4 py-2 rounded-md font-medium hover:bg-gray-100 transition">
-    {address ? `Connected` : "Connect Wallet"}
-  </button>
+          {address ? `Connected` : "Connect Wallet"}
+        </button>
       </div>
     </div>
   );
